acd4be6b85d0d276d7a279630df1afd5c9d3aa5b
codewizz <c.chaiwat.ch@gmail.com>
2022-04-04 23:07:31 +0700

    update: oauth2

--- utils/oauth2.py
+++ utils/oauth2.py
@@ -1,10 +1,15 @@
-from fastapi.security import OAuth2PasswordBearer
-from fastapi import Depends, HTTPException, status
 from typing import Optional
 from datetime import datetime, timedelta
 
+from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends, HTTPException, status
+
+from sqlalchemy.orm.session import Session
+from models.database import get_db
+from models.user import user_model
+
 from jose import jwt
-from jose import JWSError
+from jose import JWSError 
 
 from decouple import config
 
@@ -12,7 +17,6 @@ SECRET_KEY = config("SECRET_KEY")
 ALGORITHM = config("ALGORITHM")
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
 
 async def create_access_token(data: dict, expire_delta: Optional[timedelta] = None):
     to_encode = data.copy()
@@ -22,10 +26,28 @@ async def create_access_token(data: dict, expire_delta: Optional[timedelta] = No
     encode_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encode_jwt
 
-
+async def access_user_token(
+    token: str = Depends(oauth2_scheme),
+    db: Session = Depends(get_db)
+):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise await create_access_token()
+    except JWSError:
+        raise await create_access_token()
+    
 async def generate_expire_date(expire_delta: Optional[timedelta] = None):
     if expire_delta:
         expire = datetime.utcnow() + expire_delta
     else:
         expire = datetime.utcnow() + timedelta(days=1)
     return expire
+
+async def credentials_exception():
+    return HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"}
+    )
