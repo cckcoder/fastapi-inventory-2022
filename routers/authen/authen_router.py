4c8231e46e6d41f9a76668742151360db4e83615
codewizz <c.chaiwat.ch@gmail.com>
2022-04-05 07:26:45 +0700

    update: code format

--- routers/authen/authen_router.py
+++ routers/authen/authen_router.py
@@ -4,6 +4,10 @@ from fastapi.security import OAuth2PasswordRequestForm
 
 from sqlalchemy.orm.session import Session
 from models.database import get_db
+from models.user.user_model import DbUser
+
+from utils.hash import Hash
+from utils.oauth2 import access_user_token
 
 
 router = APIRouter(tags=["authentication"])
@@ -11,10 +15,20 @@ router = APIRouter(tags=["authentication"])
 
 @router.post("/login")
 async def login(
-    request: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)
+    request: OAuth2PasswordRequestForm = Depends(),
+    db: Session = Depends(get_db)
 ):
-    print("**********")
-    pprint(request.username)
-    pprint(request.password)
-    print("**********")
-    return {"Hello": "Authentication"}
+    user = db.query(DbUser).filter(DbUser.username == request.username).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid credentials")
+    elif not Hash.verify(user.password, request.password):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Incorrect password")
+    else:
+        access_token = await access_user_token(data={"sub": user.username})
+
+    return {
+        "access_token": access_token,
+        "token_type": "bearer",
+        "user_id": user.id,
+        "username": user.username
+    }
